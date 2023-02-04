a724f6c939bc830ddb0f9b900bd81e8c8a73893e
haruki301 <0301hakki869@gmail.com>
2023-02-04 15:58:11 +0900

    人参をキャンバスからスプリットに変更

--- Assets/Haru_Script/CarrotManager.cs
+++ Assets/Haru_Script/CarrotManager.cs
@@ -27,17 +27,17 @@ public class CarrotManager : MonoBehaviour
 
     void Update()
     {
-        
+        /*
         if (GameManager.Instance.IsOutGame)
 		{
             return;
-		}
+		}*/
         
         time1 += Time.deltaTime;
         time2 += Time.deltaTime;
 
 
-        Vector3 pos = new Vector3(Random.Range(-4.5f, 4.5f), 0, Random.Range(-4.5f, 4.5f));
+        Vector3 pos = new Vector3(Random.Range(-4.0f, 4.0f), 0, Random.Range(-4.0f, 4.0f));
 
         if (time1 >= wave1)
 		{
